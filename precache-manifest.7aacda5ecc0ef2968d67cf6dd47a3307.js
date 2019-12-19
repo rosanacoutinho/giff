0df85a221167544b0f1c8041cd17ce6d487f88c2
rosanacoutinho <rosanacoutinho@gmail.com>
2019-12-18 23:40:53 -0300

    Updates

---
+++ precache-manifest.7aacda5ecc0ef2968d67cf6dd47a3307.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf7545aa694efcd8b8b3c904504ac67",
+    "url": "/giff/index.html"
+  },
+  {
+    "revision": "c4bf00a70a03154a5030",
+    "url": "/giff/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ae27003c77a79416e1d6",
+    "url": "/giff/static/js/2.5692a137.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/giff/static/js/2.5692a137.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4bf00a70a03154a5030",
+    "url": "/giff/static/js/main.8b702775.chunk.js"
+  },
+  {
+    "revision": "3a37d74216f1fd2bebda",
+    "url": "/giff/static/js/runtime-main.4958d0a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/giff/static/media/logo.5d5d9eef.svg"
+  }
+]);
