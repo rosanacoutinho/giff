9c6d27431c56aac60ea335c4152dc4f43f4a84f3
rosanacoutinho <rosanacoutinho@gmail.com>
2020-01-06 15:21:48 -0300

    using bucket from aws s3

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contacts from './components/Contacts';
 
 class App extends Component {
   render() {
+    
     return (
       <Contacts contacts={this.state.contacts} />
     );
@@ -14,24 +15,25 @@ class App extends Component {
     contacts: []
   }
 
-  
-
   componentDidMount() {
-    
- 
-    fetch('https://signos-bucket.s3.us-east-2.amazonaws.com/contactlist.json',
-    {   method:'GET',
-        mode: 'cors',
-        headers:{
-            'Access-Control-Allow-Origin':'*'
-        },
-    })
+
+    //http://jsonplaceholder.typicode.com/users
+
+     //http://signos-bucket.s3.us-east-2.amazonaws.com/contactlist.json
+
+     //https://signos-bucket.s3.us-east-2.amazonaws.com/aries.txt
+      //,{ mode: 'no-cors'}
+      
+
+
+    fetch('https://signos-bucket.s3.us-east-2.amazonaws.com/contactlist.json')
     .then(res => res.json())
     .then((data) => {
       this.setState({ contacts: data })
     })
     .catch(console.log)
   }
+  
 }
 
 export default App;
